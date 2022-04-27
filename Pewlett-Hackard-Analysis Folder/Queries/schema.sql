ac29093eeeb98d76047da621e8c308896b779fcb
Robert Hutchins <rhutchins747@gmail.com>
2022-04-26 19:50:41 -0600

    Pewlett_Hackard_Retirement_Query_Complete

--- Pewlett-Hackard-Analysis Folder/Queries/schema.sql
+++ Pewlett-Hackard-Analysis Folder/Queries/schema.sql
@@ -68,3 +68,41 @@ SELECT * FROM departments; --completed
 SELECT * FROM employees; --completed
 SELECT * FROM dept_manager; --completed 
 
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT count(*)
+FROM Salaries
+
+-- Retirement eligibility
+SELECT COUNT(first_name)
+FROM  employees
+Where (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees 
+WHERE (birth_date BETWEEN '1952-01-1' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+SELECT * FROM retirement_info;
+
